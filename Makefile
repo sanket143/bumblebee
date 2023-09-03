d2b36874c4fff9e7088c43269ad90e271eae554c
Sanket <sanket143@users.noreply.github.com>
2023-09-03 16:38:06 +0530

    add document

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 all:
 	cargo run && echo && echo "############### out.json content ###############" && echo && cat out.json
+
+test:
+	cargo test -- --nocapture
--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,19 @@ use oxc::{
     parser::Parser,
     span::SourceType,
 };
+use queso::Queso;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use std::{collections::HashMap, fs::OpenOptions};
 use std::{fs::File, io::Write};
 
+mod document;
 mod evaluate;
+mod queso;
 mod types;
 
 fn main() {
-    let source_text = std::fs::read_to_string("./test.js").unwrap();
-    let writer = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(r#"out.json"#)
-        .unwrap();
-
+    let folder_name = "./test-dir";
     let allocator = Allocator::default();
-    let source_type = SourceType::from_path("index.js").unwrap();
-    let ret = Parser::new(&allocator, source_text.as_str(), source_type).parse();
-
-    let program = allocator.alloc(ret.program);
+    let queso = Queso::new(folder_name, &allocator);
 }
