e4c221840b8e830acaa89d6ec92dea7d5340599f
Sanket <sanket143@users.noreply.github.com>
2025-02-01 15:15:07 +0530

    reset commit

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,93 @@
-use crate::types::object::Serializer;
-use oxc::{
-    allocator::Allocator,
-    ast::{
-        ast::{ComputedMemberExpression, Expression, FormalParameters},
-        AstKind, Visit,
-    },
-    parser::Parser,
-    span::SourceType,
-};
-use queso::Queso;
-use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
-use std::{collections::HashMap, fs::OpenOptions};
-use std::{fs::File, io::Write};
+use anyhow::Result;
+use oxc_allocator::Allocator;
+use oxc_ast::ast::Function;
+use oxc_parser::{Parser, ParserReturn};
+use oxc_semantic::{SemanticBuilder, SemanticBuilderReturn};
+use oxc_span::SourceType;
+use oxc_traverse::{traverse_mut, Traverse};
+use std::ffi::OsStr;
+use std::path::Path;
+use walkdir::WalkDir;
 
-mod document;
-mod evaluate;
-mod queso;
-mod types;
+struct File;
 
-fn main() {
-    let folder_name = "./test-dir";
-    let allocator = Allocator::default();
-    let queso = Queso::new(folder_name, &allocator);
+impl<'a> Traverse<'a> for File {
+    fn enter_arrow_function_expression(
+        &mut self,
+        node: &mut oxc_ast::ast::ArrowFunctionExpression<'a>,
+        ctx: &mut oxc_traverse::TraverseCtx<'a>,
+    ) {
+        println!("{:#?}", &node);
+    }
+}
+
+struct Block {
+    //?
+}
+
+fn main() -> Result<()> {
+    for entry in WalkDir::new("./test-dir").into_iter().flatten() {
+        if entry.path().extension() == Some(OsStr::new("js")) {
+            println!("{}", entry.path().display());
+            let source_path = entry.path();
+            let source_text = std::fs::read_to_string(entry.path())?;
+            let allocator = Allocator::default();
+            let source_type = SourceType::from_path(source_path)?;
+            let mut file = File {};
+
+            let mut errors = Vec::new();
+            let ParserReturn {
+                mut program,
+                errors: parsing_errors,
+                panicked,
+                ..
+            } = Parser::new(&allocator, source_text.as_str(), source_type).parse();
+
+            errors.extend(parsing_errors);
+
+            if panicked {
+                for error in &errors {
+                    eprintln!("{error:?}");
+                }
+
+                panic!("Parsing failed.");
+            }
+
+            let SemanticBuilderReturn {
+                semantic,
+                errors: semantic_errors,
+            } = SemanticBuilder::new().build(&program);
+
+            errors.extend(semantic_errors);
+
+            if errors.is_empty() {
+                println!("parsing and semantic analysis completed successfully.");
+            } else {
+                for error in errors {
+                    eprintln!("{error:?}");
+                }
+
+                panic!("Failed to build Semantic for Counter component.");
+            }
+
+            // println!("{:#?}", program);
+
+            let (symbol_table, scope_tree) = semantic.into_symbol_table_and_scope_tree();
+
+            traverse_mut(
+                &mut file,
+                &allocator,
+                &mut program,
+                symbol_table,
+                scope_tree,
+            );
+            // we need to process each file
+            // keep track of function call references
+            // - in other functions
+            // - other object functions
+            // - callbacks?
+        }
+    }
+
+    Ok(())
 }
---
+++ test-dir/factory.js
@@ -0,0 +1,9 @@
+const factory = {
+  test: () => {
+    console.log("test");
+  },
+};
+
+export const call = () => {
+  factory.test();
+};
